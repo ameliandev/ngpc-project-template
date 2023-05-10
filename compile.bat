630df44789b011f1c55f86b838fc6de0811c4a49
Adrián Melián <adrianmgdev@gmail.com>
2023-05-10 23:17:09 +0100

    DEV - Improvements on makefile and compile file.

--- compile.bat
+++ compile.bat
@@ -2,31 +2,51 @@
 
 cls
 
-::Var declarations
+REM *****************************************************
+REM *                   CONSTS                          *
+REM *                                                   *
+REM *     Configure the 'optional' constants first      *
+REM *****************************************************
+
+REM (1==true, 0==false) Resize Rom converts the rom size to 2Mb for use in flash carts. This is because flash carts only allow roms bigger than 2Mb. Not needed if your rom is bigger. 
+SET ResizeRom=1
+REM (1==true, 0==false) Run allows to run the compiled rom through the established emulator.
+SET Run=1
+REM The ngc file name. If you want to change the cartridge rom name, change it on CartTitle var into carthdr.h file.
 SET romName=main
-SET rootPath=%~dp0
+REM Emulator path. If you put Run const equals 1, this var value must exists
+SET emuPath="%NGPEMU%\NeoPop\NeoPop-Win32.exe"
+@REM SET emuPath="%NGPEMU%\BizHawk-2.2.1\EmuHawk.exe"
+@REM SET emuPath="%NGPEMU%\ares-v122\lucia.exe"
+
+
+REM *****************************************************
+REM *                 COMPILER CONST                    *
+REM *****************************************************
+
+REM THOME must be the location of your ngpcbins\T900 compiler folder. By default C:\ngpcbins\T900
 SET THOME=C:\ngpcbins\T900
-SET emuPath="%NGPEMU%\BizHawk-2.2.1\EmuHawk.exe"
-@REM set emuPath="%NGPEMU%\NeoPop\NeoPop-Win32.exe"
-@REM set emuPath="%NGPEMU%\ares-v122\lucia.exe"
+SET BinPath=bin
+SET rootPath=%~dp0
 
-path=%path%;C:\ngpcbins\T900\bin
 
-::Borrado previo--------------------------------------------------------------------
-SET list=ngp ngc rel abs s24 map sav 
-(for %%a in (%list%) do ( 
-    if exist .\bin\*.%%a del .\bin\*.%%a > nul
-))
+REM *****************************************************
+REM *                  COMPILATION                      *
+REM *****************************************************
 
-del /S *.rel > nul
+path=%path%;%THOME%\bin
 
+make winclean
 make
+make winmove_files
 
-::Movemos binarios------------------------------------------------------------------
-if exist %romName%.ngp rename %romName%.ngp %romName%.ngc
-(for %%a in (%list%) do ( 
-    if exist %romName%.%%a move %romName%.%%a ".\bin\%romName%.%%a" > nul
-))
+if "%ResizeRom%"=="1" (
+    MOVE "%~dp0%BinPath%\%romName%.ngc" "%~dp0%BinPath%\_%romName%.ngc" > nul
+    "%~dp0utils\NGPRomResize.exe" "%~dp0%BinPath%\_%romName%.ngc" > nul
+    MOVE "%~dp0%BinPath%\_%romName%.ngc" "%~dp0%BinPath%\%romName%.ngc" > nul
+    DEL "%~dp0%BinPath%\2MB__%romName%.ngc" > nul
+)
 
-::Ejecutamos------------------------------------------------------------------------
-%emuPath% %rootPath:~0,-1%.\bin\%romName%.ngc
+if "%Run%"=="1" (
+    %emuPath% %rootPath:~0,-1%.\%BinPath%\%romName%.ngc
+)
--- makefile
+++ makefile
@@ -1,10 +1,12 @@
 .SUFFIXES: .c .asm .rel .abs
-NAME = MAIN
+NAME = main
 OBJS = \
 	main.rel \
 	library.rel \
 	core/rom/flash.rel \
-	
+
+CURRENT_DIR := $(abspath .)
+OUTPUT_DIR = bin
 
 $(NAME).ngp: makefile ngpc.lcf $(OBJS)
 	tulink -la -o $(NAME).abs ngpc.lcf system.lib $(OBJS)
@@ -14,8 +16,18 @@ $(NAME).ngp: makefile ngpc.lcf $(OBJS)
 .c.rel:
 	cc900 -c -O3 $< -o $@
 
-clean:
-	del *.rel
-	del *.abs
-	del *.map 
-	del *.s24
+winclean:
+	
+	@rm -f $(OUTPUT_DIR)/*.abs 
+	@rm -f $(OUTPUT_DIR)/*.map
+	@rm -f $(OUTPUT_DIR)/*.s24
+	@rm -f $(OUTPUT_DIR)/*.ngc
+	@rm -f $(OUTPUT_DIR)/*.ngp
+	@rm -rf *.rel
+
+winmove_files:
+	@if [ ! -d "$(OUTPUT_DIR)" ]; then mkdir $(OUTPUT_DIR); fi
+	@if [ -f "$(NAME).ngp" ]; then mv $(NAME).ngp $(OUTPUT_DIR)/$(NAME).ngc; fi
+	@if [ -f "$(NAME).abs" ]; then mv $(NAME).abs $(OUTPUT_DIR)/$(NAME).abs; fi
+	@if [ -f "$(NAME).rel" ]; then mv $(NAME).rel $(OUTPUT_DIR)/$(NAME).rel; fi
+	@if [ -f "$(NAME).s24" ]; then mv $(NAME).s24 $(OUTPUT_DIR)/$(NAME).s24; fi
