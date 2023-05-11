a5a30c7a5896b92846f95c53eaa9437fe26289c6
Adrián Melián <adrianmgdev@gmail.com>
2023-05-11 21:07:50 +0100

    DEV - Unix makefile and compile script update.

--- compile.sh
+++ compile.sh
@@ -1,12 +1,27 @@
 #!/bin/bash
 
-# vars declaration
+# *****************************************************
+# *                   CONSTS                          *
+# *                                                   *
+# *     Configure the 'optional' constants first      *
+# *****************************************************
+
+# (1==true, 0==false) Resize Rom converts the rom size to 2Mb for use in flash carts. This is because flash carts only allow roms bigger than 2Mb. Not needed if your rom is bigger. 
+ResizeRom=1
+# (1==true, 0==false) Run allows to run the compiled rom through the established emulator.
+Run=1
+# The ngc file name. If you want to change the cartridge rom name, change it on CartTitle var into carthdr.h file.
+romName='main'
+
 corePath='/home/[username]/snap/retroarch/current/.config/retroarch/cores'
 compileSuccess=1
 error=0
 emu_neopop='race_libretro.so'
 emu_mednafe='mednafen_ngp_libretro.so'
-romName='main'
+
+# *****************************************************
+# *                 COMPILER CONST                    *
+# *****************************************************
 romNameOut=$romName'.ngc'
 compilerPath=/home/[username]/ngpcbins/T900
 
@@ -17,13 +32,15 @@ if [[ ! $PATH =~ ^$compilerPath ]]; then
 fi
 
 # removing all .rel files
-find . -name "*.rel" | xargs rm 
+# find . -name "*.rel" | xargs rm 
 
 # cleaning ./bin directory
-rm -r ./bin/* &> /dev/null
+# rm -r ./bin/* &> /dev/null
 
 # compiling
+make clean
 make
+make move_files
 
 # on error, exit
 if [ ! -e $romName'.ngp' ]; then
@@ -31,17 +48,21 @@ if [ ! -e $romName'.ngp' ]; then
     exit
 fi
 
+if [ "$Run" -eq 1 ]; then
+    retroArchCmd='retroarch --libretro '$corePath'/'$emu_mednafe' ./bin/'$romNameOut
+    $retroArchCmd
+fi
+
 # moving compiled files to ./bin directory
-for file in "$arg"./{$romName'.map',$romName'.abs',$romName'.s24',$romName.'ngp',$romName'.ngc',$romName.'rel'} ; do
-    if [ -e $file ]; then
-        if [ $file = "./"$romName'.ngp' ]; then
-            mv $file ./bin/$romName.'ngc'
-        else
-            mv $file ./bin/$file
-        fi
-    fi
-done
+# for file in "$arg"./{$romName'.map',$romName'.abs',$romName'.s24',$romName.'ngp',$romName'.ngc',$romName.'rel'} ; do
+#     if [ -e $file ]; then
+#         if [ $file = "./"$romName'.ngp' ]; then
+#             mv $file ./bin/$romName.'ngc'
+#         else
+#             mv $file ./bin/$file
+#         fi
+#     fi
+# done
 
 # launching rom on emulator
-retroArchCmd='retroarch --libretro '$corePath'/'$emu_mednafe' ./bin/'$romNameOut
-$retroArchCmd
+
